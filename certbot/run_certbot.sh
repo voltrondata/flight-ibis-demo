9fb73f0b1b26369f5936d053f02b2fde7fa23c25
prmoore77 <prmoore77@hotmail.com>
2023-05-01 17:29:55 -0400

    Its alive!

--- certbot/run_certbot.sh
+++ certbot/run_certbot.sh
@@ -2,6 +2,8 @@
 
 certbot certonly --manual \
                  --preferred-challenges=dns \
+                 --email=philip@voltrondata.com \
+                 --key-type=rsa \
                  --work-dir=work \
                  --logs-dir=logs \
                  --config-dir=config
